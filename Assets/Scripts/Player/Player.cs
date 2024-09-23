2fe7cd22a36c001bd0259ef44747cde5cda1d555
LIt4g0 <lit4g0@gmail.com>
2024-09-23 11:49:17 +0200

    Player and projectiles

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int currentLane = 0;
+    [SerializeField] float laneYSize = 5;
+    [SerializeField] Projectile projectile;
+    [SerializeField] StompWave stompWave;
+
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Vertical"))
+        {
+            currentLane = Mathf.Clamp(currentLane + (int)Input.GetAxisRaw("Vertical"), 0, 2);
+            //Debug.Log(currentLane);
+            transform.position = new Vector3(transform.position.x, (currentLane-1) * laneYSize);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 spawnPos = transform.position;
+            Instantiate(stompWave, spawnPos, Quaternion.identity);
+            Debug.Log("STOMP!");
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 spawnPos = transform.position;
+            Instantiate(projectile, spawnPos, Quaternion.identity);
+            Debug.Log("Fire!!");
+        }
     }
 }
