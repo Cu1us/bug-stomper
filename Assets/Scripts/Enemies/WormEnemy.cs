d02414b90d889f7f6427d71e4d9cee50b0e27c57
Måns Fritiofsson <mans@fritiofson.net>
2024-09-25 13:49:13 +0200

    Fix abstract class

--- Assets/Scripts/Enemies/WormEnemy.cs
+++ Assets/Scripts/Enemies/WormEnemy.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class WormEnemy : Enemy
 {
-    
+    public override Projectile.ReturnBehavior HitByProjectile(int damage)
+    {
+        return Projectile.ReturnBehavior.HIT;
+    }
+
+    public override void HitByShockwave()
+    {
+        
+    }
 }
