36ce3fa25a95904185a8c1c0839178b28a552520
Måns Fritiofsson <mans@fritiofson.net>
2024-09-26 15:09:38 +0200

    Worm flip

--- Assets/Scripts/Enemies/WormEnemy.cs
+++ Assets/Scripts/Enemies/WormEnemy.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class WormEnemy : Enemy
 {
-    [SerializeField]
-    float flipAnimationTime;
-    [SerializeField]
-    AnimationCurve flipAnimationCurve;
 
-    float flippedTime = 0;
-    [SerializeField]
-    float timeTakenToUnflip = 2f;
     public override Projectile.ReturnBehavior HitByProjectile(int damage)
     {
         Damage(damage);
@@ -21,35 +14,23 @@ public class WormEnemy : Enemy
     public override void HitByShockwave()
     {
         Flip();
-        flippedTime = 0;
-    }
-    void Update()
-    {
-        flippedTime += Time.deltaTime;
-        if (flipped && flippedTime > timeTakenToUnflip)
-        {
-            Flip();
-        }
-
-        float flipProgress = Mathf.Clamp((Time.time - lastFlipTime) / flipAnimationTime, 0f, 1f);
-        flipProgress = flipAnimationCurve.Evaluate(flipProgress);
-        if (flipped)
-        {
-            transform.eulerAngles = new Vector3(0, 0, -180 * flipProgress);
-        }
-        else
-        {
-            transform.eulerAngles = new Vector3(0, 0, 180 - 180 * flipProgress);
-        }
     }
 
     public override void Move()
     {
-        if (!flipped && (Time.time - lastFlipTime) > flipAnimationTime / 2) base.Move();
+        if (!flipped)
+            base.Move();
     }
     public override void Flip()
     {
+        if (flipped) return;
+        flipped = true;
         animator.Play("Flip");
-        base.Flip();
+    }
+
+    public void OnFlipFinish()
+    {
+        flipped = false;
+        animator.Play("Walk");
     }
 }
